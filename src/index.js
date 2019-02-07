e203b914771f579ded1664a7bae2b82301362569
Shreya Verma <svshreya97@gmail.com>
2019-02-07 09:38:23 +0530

    Added filterable product table to landing page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import FilterableProductTable from './Components/FilterableProductTable';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const productArray = [
+    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+  ];
+const productArraySortedByCategory = productArray.slice().sort((product1, product2) => (product2.category > product1.category) ? 1 : ((product1.category > product2.category) ? -1 : 0));
+ReactDOM.render(<FilterableProductTable productArray= {productArraySortedByCategory} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
